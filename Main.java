95590eb3d8da3ba4461afe1f20ac85fa5e2a41b7
Nandita24 <69765570+Nandita24@users.noreply.github.com>
2021-08-06 20:07:54 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,63 +1,63 @@
-/*1. 
-a. Create a Java class called Student with the following details as variables within it. 
-(i) USN 
-(ii) Name 
-(iii) Programme 
-(iv) Phone 
-Write a Java program to create nStudent objects and print the USN, Name, Programme, and 
-Phoneof these objects with suitable headings.
-*/
+/*
+2b.	Write a Java class called Customer to store their name and date_of_birth. The date_of_birth 
+	format should be dd/mm/yyyy. Write methods to read customer data as <name, 
+	dd/mm/yyyy> and display as <name, dd, mm, yyyy> using StringTokenizer class 
+	considering the delimiter character as “/”. 
+ */
+import java.util.NoSuchElementException;
 import java.util.Scanner;
-class Student {
-	private String usn,name,branch,ph;
-	
-	public void accept()
+import java.util.StringJoiner;
+import java.util.StringTokenizer;
+
+class Customer{
+	String name;
+	String dob;
+	void accept()
 	{
 		Scanner scanner=new Scanner(System.in);
-		
-		System.out.println("Enter USN\n");
-		usn=scanner.next();
-		
-		System.out.println("Enter Name\n");
-		name=scanner.next();
-		
-		System.out.println("Enter Branch\n");
-		branch=scanner.next();
-		
-		System.out.println("Enter Phone number\n");
-		ph=scanner.next();
-	}
-	
-	public void display()
-	{
-		System.out.println("Student details\n");
-		System.out.println("Usn: "+usn);
-		System.out.println("Name: "+name);
-		System.out.println("Branch: "+branch);
-		System.out.println("Phone: "+ph);
-		System.out.println();
-	}
-
-}
-
-public class Main {
-	public static void main(String[] args)
-	{
-		Scanner scanner=new Scanner(System.in);
-		
-		System.out.println("Enter value for n");
-		int n=scanner.nextInt();
-		
-		Student[] ob1=new Student[n];
-		
-		for(int i=0;i<n;i++)
-		{
-			ob1[i]=new Student(); //Intializing obj using constructors
-			ob1[i].accept();
+		System.out.println("Enter Name and DOB");
+		String input=scanner.nextLine();
+		StringTokenizer stringTokenizer=new StringTokenizer(input,",");
+		try {
+			name=stringTokenizer.nextToken().trim();
+			dob=stringTokenizer.nextToken().trim();
 		}
+		catch(NoSuchElementException e) {
+			System.out.println("Invalid Format\n");
+			System.exit(0);
+		}
+	}
+
+	
+	void display() {
+		StringTokenizer stringTokenizer=new StringTokenizer(dob,"/");
+		try {
+			String dd=stringTokenizer.nextToken().trim();
+			String mm=stringTokenizer.nextToken().trim();
+			String yy=stringTokenizer.nextToken().trim();
+			
 		
-		for(int i=0;i<n;i++)
-				ob1[i].display();
+		StringJoiner stringJoiner =new StringJoiner(", ");
+		stringJoiner.add(name).add(dd).add(mm).add(yy);
+		
+		System.out.println(stringJoiner.toString());
+	}
+	catch(NoSuchElementException e) {
+		System.out.println("Invalid Format\n");
+		System.exit(0);
+		
+		
 	}
 }
+}
+public class Main {
+	static Customer customer;
 	
+	public static void main(String[] args) {
+		customer=new Customer();
+		customer.accept();
+		customer.display();
+		
+		
+	}
+}
